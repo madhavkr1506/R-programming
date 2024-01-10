1bb62c21e203dd97095f9b7149c8f67c158d33d7
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-01-11 01:41:13 +0530

    Add files via upload

---
+++ Program/Rprogramming1.R
@@ -0,0 +1,39 @@
+# Note1: R is a case sensitive language;
+
+# R is a open source programming language;
+# R is a interpreted programming language;
+# It is also a software environment used to analyze statistical information, graphical representation, reporting, and data modeling.
+# R is the implementation of the s programming language, which is combined with lexical scoping semantics;
+# 
+# History : 
+# R was developed by Ross Ihaka and Robert Gentleman int the university of Auckland, New Zealand;
+# The first project was considered in the 1992;
+# The initial version was released in the 1995;
+# In 2000 a stable beta version was released;
+# latest version of R is version 4.3.2 which has been released in 2023;
+# 
+# 
+# Features : 
+# powerful graphics;
+# No need of compiler;
+# cross platform support;
+# perform fast calculation;
+# It is platform Independent;
+# It can integrate with other language;
+# distributed computing;
+
+
+print("Hello R programming language");
+String1<-"Hello madhav"
+print(String1);
+# --> This is a single line comment;
+sessionInfo();
+#  --> this will help us to know that which package are attached with base package;
+
+#lets know how to plot graph : 
+x<-1:10
+plot(x);
+
+?plot;
+# Help on topic 'plot' was found in the following packages:
+
