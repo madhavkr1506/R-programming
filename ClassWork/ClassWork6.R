69e6cdefc4442281d85853965accc00486f619a8
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-01-22 01:03:12 +0530

    Add files via upload

---
+++ ClassWork/ClassWork6.R
@@ -0,0 +1,85 @@
+# write a program to print vector1 element
+
+vector1<-c(11,20,56,45,68,89,56)
+print(vector1)
+
+# sort vector1
+sort(vector1)
+
+# original position of element
+order(vector1)
+
+# It will print sorted vector element
+vector1[order(vector1)]
+
+# access multiple vector1 elements
+vector1[c(1,3,2,6)]
+
+# ranking of vector1 : indexing
+
+rank(vector1)
+
+vector1[rank(vector1)]
+
+# make a data frame : 
+
+dataFrame1<-data.frame(
+  name=c("Madhav","Kishan","Rahul","Rohan","Satyam"),
+  RegNo=c(1,2,3,4,5)
+)
+
+# use view function to view dataFrame1 : 
+View(dataFrame1)
+
+
+# write into csv file : 
+write.csv(dataFrame1,"dataFrame1.csv")
+
+# read from csv file : 
+csv_data<-read.csv(file = "dataFrame1.csv")
+View(csv_data)
+
+# another way to read csv file : 
+
+csv_data<-read.table(file = "dataFrame1.csv",header = T,sep = ",")
+View(csv_data)
+
+
+# directly edit file : use wisely
+fix(csv_data)
+
+# to know the structures of csv file
+str(csv_data)
+
+# summary of csv file
+summary(csv_data)
+
+# print all variable names
+names(csv_data)
+
+# length function
+length(csv_data)
+
+# dimension function
+dim(csv_data)
+
+# colnames() function
+colnames(csv_data)
+
+# get first six row details
+head(csv_data)
+
+# get last six row details
+tail(csv_data)
+
+# access csv file : first two rows
+csv_data<-csv_data[c(1:2),]
+print(csv_data)
+
+# access csv file : first two rows and two columns
+csv_data<-csv_data[c(1:2),c(1:2)]
+print(csv_data)
+
+
+
+
