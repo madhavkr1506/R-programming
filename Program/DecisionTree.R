6f0f21b8b7e019701bf6325b8368e17fd3ce924c
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-10-09 15:57:48 +0530

    Add files via upload

--- Program/DecisionTree.R
+++ Program/DecisionTree.R
@@ -7,7 +7,7 @@ library(rpart.plot)
 data(iris)
 str(iris)
 
-set.seed(42)
+# set.seed(42) 
 
 indexes<-sample(1:nrow(iris),0.7*nrow(iris))
 
@@ -32,3 +32,64 @@ accuracy = sum(diag(confusion_matrix)) / sum(confusion_matrix)
 
 
 print(paste("Accuracy: ",round(accuracy , 4)))
+
+
+
+summary(iris)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# intrusion detection dataset
+
+data<-read.csv("intrusion_detection_data.csv")
+View(data)
+data$protocol_type<-as.integer(as.factor(data$protocol_type))
+View(data)
+
+data$service<-NULL
+data$flag<-NULL
+
+indexes = sample(1:nrow(data),0.7 * nrow(data))
+traindata<-data[indexes,]
+testdata<-data[-indexes,]
+
+target = class ~.
+
+library(rpart)
+library(rpart.plot)
+
+
+tree = rpart(target, data = traindata, method = "class")
+
+rpart.plot(tree)
+
+predictions<- predict(tree, testdata, type = "class")
+
+confusion_matrix = table(testdata$class,predictions)
+
+print(confusion_matrix)
+
+accuracy <- sum(diag(confusion_matrix)) / sum(confusion_matrix)
+
+print(paste("Accuracy: ",round(accuracy,4)))
