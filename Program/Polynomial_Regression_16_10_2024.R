fd2a6600029778ae5cc06e7e435882d4072d3173
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-10-17 19:36:56 +0530

    Add files via upload

--- Program/Polynomial_Regression_16_10_2024.R
+++ Program/Polynomial_Regression_16_10_2024.R
@@ -1,40 +1,62 @@
-data = read.csv("Position_Salaries.csv")
+
+data<- read.csv("Position_Salaries.csv")
 View(data)
+data$Position <- NULL
 
 
-data$Position<-NULL
-
-View(data)
-
-lin_reg = lm(formula = Salary ~., data = data)
+lin_reg <- lm(formula=Salary ~.,data = data)
 summary(lin_reg)
 
 
-data$level2 = data$Level^2
-data$level3 = data$Level^3
-data$level4 = data$Level^4
-data$level5 = data$Level^5
 
-data
+data$level2<- data$Level^2
+data$level3<- data$Level^3
+data$level4<- data$Level^4
 
-
-poly_reg = lm(formula = Salary ~., data = data)
-
-
+poly_reg<- lm(formula = Salary~.,data = data)
 summary(poly_reg)
 
-data$Level
-
-data$level2
 data$level3
 data$level4
-data$level5
+
 
 library(ggplot2)
 
+ggplot() + geom_point(aes(x=data$Level,y=data$Salary),colour = 'red') + 
+  geom_line(aes(x=data$Level,y=predict(lin_reg,newdata = data)),colour = 'blue')+
+  ggtitle('Truth or Bluff(Linear Regression)') + xlab('Level') + ylab('Salary')
 
-ggplot() + geom_point(aes(x = data$level, y = data$Salary),
-                      )
 
 
-# decision tree, linear, polynomial, knn, naive bayes : syllabus for ca2
+ggplot() + geom_point(aes(x=data$level2,y=data$Salary),color = "purple") +
+  geom_line(aes(x=data$level2,y=predict(poly_reg,newdata = data)),color="green")+
+  ggtitle('Polynomial linear regression') + xlab('Level') + ylab('Salary')
+
+ggplot() + geom_point(aes(x=data$level3,y=data$Salary),color = "purple") +
+  geom_line(aes(x=data$level3,y=predict(poly_reg,newdata = data)),color="green")+
+  ggtitle('Polynomial linear regression') + xlab('Level') + ylab('Salary')
+
+ggplot() + geom_point(aes(x=data$level4,y=data$Salary),color = "purple") +
+  geom_line(aes(x=data$level4,y=predict(poly_reg,newdata = data)),color="green")+
+  ggtitle('Polynomial linear regression') + xlab('Level') + ylab('Salary')
+
+
+ggplot() + geom_point(aes(x=data$Level,y=data$Salary),color = "purple") +
+  geom_line(aes(x=data$Level,y=predict(poly_reg,newdata = data)),color="green")+
+  ggtitle('Polynomial linear regression') + xlab('Level') + ylab('Salary')
+
+summary(poly_reg)
+
+
+x_grid = seq(min(data$Level),max(data$Level),0.1)
+ggplot() + geom_point(aes(x=data$Level,y=data$Salary),colour='pink')+
+  geom_line(aes(x = x_grid,y=predict(poly_reg,newdata=data.frame(Level=x_grid,
+                                                                 level2=x_grid^2,level3=x_grid^3,level4=x_grid^4)))) + ggtitle('Smothning curve')+xlab('Level')+ylab('Salary')
+
+
+
+predict(lin_reg,data.frame(Level=7))
+
+
+
+predict(poly_reg,data.frame(Level=7,level2=7^2,level3=7^3,level4=7^4))
