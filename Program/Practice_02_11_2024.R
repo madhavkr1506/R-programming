a005427f4a583eba8de15dd47b603d0b56913830
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-11-02 00:13:16 +0530

    Add files via upload

---
+++ Program/Practice_02_11_2024.R
@@ -0,0 +1,68 @@
+# KNN
+
+library(mlbench)
+data("Sonar")
+data = as.data.frame(Sonar)
+
+head(data)
+
+normalize = function(x){
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+data_norm = as.data.frame(lapply(data[,-ncol(data)],normalize))
+
+head(data_norm)
+
+data_norm$Class = data$Class
+
+sample_ = sample(1:nrow(data_norm) , 0.7 * nrow(data_norm))
+
+train_data = data_norm[sample_,-ncol(data_norm)]
+test_data = data_norm[-sample_,-ncol(data_norm)]
+
+train_label = data_norm[sample_,ncol(data_norm)]
+test_label = data_norm[-sample_,ncol(data_norm)]
+
+library(class)
+library(caret)
+library(gmodels)
+
+pred_model = knn(train = train_data, test = test_data, cl = train_label, k = 6)
+
+eval_model = confusionMatrix(pred_model, test_label)
+eval_model
+
+CrossTable(x = pred_model, y = test_label, prop.chisq = FALSE)
+
+
+# NAIVE BAYES
+
+
+library(mlbench)
+data("Sonar")
+data = as.data.frame(Sonar)
+
+head(data)
+
+sample_ = sample(1:nrow(data), 0.7 * nrow(data))
+
+train_data = data[sample_,]
+test_data = data[-sample_,]
+
+library(e1071)
+
+model = naiveBayes(Class ~., data = train_data)
+
+pred_model = predict(model, test_data)
+
+pred_model
+
+library(caret)
+library(gmodels)
+
+confusionMatrix(pred_model, test_data$Class)
+
+CrossTable(x = pred_model, y = test_data$Class, prop.chisq = FALSE)
+
+
