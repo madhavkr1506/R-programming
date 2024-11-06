3f09a77eb488efa5db9fc8a097be2439cf9c0ec4
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-11-06 15:14:45 +0530

    Add files via upload

---
+++ Program/SVM_06_11_2024.R
@@ -0,0 +1,30 @@
+data = read.csv("social.csv")
+data = data[3:5]
+data$Purchased = factor(data$Purchased, levels = c(0,1))
+View(data)
+
+sample_ = sample(1:nrow(data), 0.75 * nrow(data))
+
+train_data = data[sample_,]
+test_data = data[-sample_,]
+
+library(e1071)
+
+classify = svm(formula = Purchased ~., data = train_data, type = "C-classification", kernel = "linear")
+
+pred_model = predict(classify, test_data[,-3])
+pred_model
+
+library(caret)
+
+confusionMatrix(pred_model, test_data$Purchased)
+
+
+# what is scale: scale is a generic function whose default method centers and /or scales the columns of numeric matrix.
+
+# normalization : it aims for specific range (usually 0 - 1) and change the shape of data distribution.
+
+# scaling:  adjust the spread or variability of the data without necessarily aiming for specific range. it focuses on making features equally important for machine algorithm.
+
+
+
