876654967a025d0d60e61343ae48eb479fe5f832
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-11-07 15:50:43 +0530

    Add files via upload

--- Program/Clustering_07_11_2024.R
+++ Program/Clustering_07_11_2024.R
@@ -18,3 +18,28 @@ accuracy = sum(diag(confusionTable)) / sum(confusionTable)
 
 
 print(paste("Accuracy: ",accuracy))
+
+
+plot(data_[c("Sepal.Length","Sepal.Width")],
+     col = kmeans.re$cluster,
+     main = "K-means with 3 clusters")
+
+
+points(kmeans.re$centers[,c("Sepal.Length","Sepal.Width")],
+       col = 1:3,
+       pch = 8, cex = 3)
+
+ykmeans = kmeans.re$cluster
+
+clusplot(data_[,c("Sepal.Length","Sepal.Width")],
+         ykmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste("Cluster Iris"),
+         xlab = "Sepal.Length",
+         ylab = "Sepal.Width"
+)
