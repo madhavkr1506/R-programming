79a731057887d468d849de84af40376d855af8a2
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-12-03 17:39:46 +0530

    Add files via upload

--- Program/Practice_02_12_2024.R
+++ Program/Practice_02_12_2024.R
@@ -92,3 +92,95 @@ clusplot(data_[,c("carat","price")],
          ylab = "carat"
 )
 
+
+# cluster dendrogram
+
+d = dist(scale_data, method = "euclidean")
+hfit = hclust(d)
+plot(hfit)
+
+graph = cutree(hfit, k = 4)
+graph
+
+rect.hclust(hfit, k = 4, border = "red")
+
+
+# random forest
+
+library(ggplot2)
+data = as.data.frame(diamonds)
+data = data[1:1000,]
+
+sample_ = sample(1:nrow(data), 0.78 * nrow(data))
+
+train_data = data[sample_,]
+test_data = data[-sample_,]
+
+library(randomForest)
+
+rf.model = randomForest(cut ~., data = train_data, ntree = 100)
+rf.model
+
+rf.model.predict = predict(rf.model,test_data)
+rf.model.predict
+
+confusion_table = table(rf.model.predict,test_data$cut)
+confusion_table
+
+accuracy = sum(diag(confusion_table)) / sum(confusion_table)
+accuracy
+
+
+
+# market basket analysis
+
+library(arules)
+
+data("Groceries")
+
+summary(Groceries)
+str(Groceries)
+
+inspect(Groceries[1:5])
+
+itemFrequency(Groceries[,1:3])
+itemFrequencyPlot(Groceries)
+itemFrequencyPlot(Groceries,support = 0.1)
+itemFrequencyPlot(Groceries, topN = 20)
+
+
+
+image(Groceries[1:3])
+image(sample(Groceries,100))
+
+
+apriori(Groceries)
+
+
+groceryrules = apriori(Groceries, parameter = list(support = 0.006, confidence = 0.25, minlen = 2))
+
+inspect(groceryrules[1:3])
+groceryrules
+
+summary(groceryrules)
+
+
+inspect(groceryrules[1:10], lift = 0.5)
+inspect(sort(groceryrules, by = "lift")[1:5])
+
+berryrules = subset(groceryrules,items %in% "berries")
+inspect(berryrules)
+
+sodarules = subset(groceryrules, rhs %in% "soda")
+inspect(sodarules)
+
+top.soda.rules = head(sort(sodarules, by = "lift"),5)
+inspect(top.soda.rules)
+
+
+write(groceryrules, "groceryrules_03_12_24.csv", sep = ",", quote = TRUE, row.names = FALSE)
+
+groceryrules_df = read.csv("groceryrules_03_12_24.csv")
+str(groceryrules_df)
+
+
